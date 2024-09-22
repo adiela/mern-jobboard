87942e8c46861b7c41fc135e77d5e4ed5abf7d4b
Adiela Abishua <adiela.abishua@gmail.com>
2024-09-22 23:11:26 +0300

    Add DB config

--- backend/server.js
+++ backend/server.js
@@ -1,4 +1,8 @@
 import express from 'express';
+import dotenv from 'dotenv';
+import { connectDB } from './config/db.js';
+
+dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 3000;
@@ -8,5 +12,6 @@ app.get('/', (req, res) => {
 });
 
 app.listen(PORT, () => {
+    connectDB();
     console.log(`Server is running on port ${PORT}`);
 });
