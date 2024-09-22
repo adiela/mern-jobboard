70e8180587241a12132377f652f73ced1c8a0dbb
Adiela Abishua <adiela.abishua@gmail.com>
2024-09-23 01:30:18 +0300

    Create home page with dummy data

---
+++ frontend/src/components/jobList.js
@@ -0,0 +1,28 @@
+import React from 'react';
+
+const JobList = ({ jobs }) => {
+    return (
+        <ul>
+            {jobs.map((job, index) => (
+                <li key={index} className="job-item border-b-2 mb-5 py-5 flex flex-col">
+                    <div className="flex justify-between">
+                        <h2 className="font-bold text-xl">{job.title}</h2>
+                        <span>{job.type}</span>
+                    </div>
+                    
+                    <div className="flex justify-between text-sm text-slate-600">
+                        <p><strong>Company:</strong> {job.company}</p>
+                        <p><strong>Location:</strong> {job.location}</p>
+                    </div>
+                    <div className="flex justify-between text-sm text-slate-600 items-end mt-3">
+                        <p className="text-sm">Date posted: {job.date}</p>
+                        <a href="#" className="text-sm bg-sky-500 text-white px-2 py-1.5 rounded-md">View Job Details</a>
+                    </div>
+                    
+                </li>
+            ))}
+        </ul>
+    );
+};
+
+export default JobList;
