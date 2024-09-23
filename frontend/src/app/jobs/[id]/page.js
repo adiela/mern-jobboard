7a3af181b6d867ed32f42b84770d6cc259c9ba9c
Adiela Abishua <adiela.abishua@gmail.com>
2024-09-23 12:26:04 +0300

    Removed relative path imports

--- frontend/src/app/jobs/[id]/page.js
+++ frontend/src/app/jobs/[id]/page.js
@@ -1,7 +1,7 @@
 "use client"
 import { useState, useEffect } from 'react';
-import JobForm from '../../../components/jobForm';
-import JobPost from '../../../components/JobPost';
+import JobForm from '@/components/jobForm';
+import JobPost from '@/components/JobPost';
 import { useRouter } from 'next/navigation'
 
 
